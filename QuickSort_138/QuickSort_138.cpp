0c176a2f2a83a88eb90119ef947d91f39fbbd433
NilaAyu1611 <nilaayucw@gmail.com>
2023-03-30 15:02:36 +0700

    prosedur input

--- QuickSort_138/QuickSort_138.cpp
+++ QuickSort_138/QuickSort_138.cpp
@@ -5,3 +5,8 @@ using namespace std;
 int arr[20];			//array of integers to hold values
 int cmp_count = 0;		// number of comparasion
 int mov_count = 0;		// number of data movements
+int n;
+
+void input(); {
+	while (true)
+}
